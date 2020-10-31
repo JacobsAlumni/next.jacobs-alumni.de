b759f75f5927c48fec079b96f299bd9372a79938
Tom Wiesing <tkw01536@gmail.com>
2020-10-31 14:36:20 +0100

    Add image to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import Layout from "../layout/index";
+import Image from 'next/image';
 
 export default class Home extends React.Component {
     render() {
         return <Layout>
-            This is the new jacobs-alumni.de website, rewritten in NextJS. ]
-            Still working on it, but I'm testing the layout. 
+
+            <figure className="uk-width-1-1">
+                <Image src="/media/people.jpg" alt="Jacobs Alumni Group Picture from Homecoming 2019" width={4032} height={3024}/>
+            </figure>
+
+            <div className="uk-clearfix">
+                <p className="uk-text-lead" style={{textAlign: "center"}}>
+                    The Jacobs Alumni Association connects and represents the interests of its alumni and provides benefits and opportunities for them and Jacobs University to thrive.
+                </p>
+            </div>
         </Layout>
     }
 }
