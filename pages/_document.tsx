98233821ada6b759de77c933d7bd880b08e1fc4d
Leonhard Kuboschek <leo@jacobs-alumni.de>
2021-05-02 16:51:58 +0200

    Add analytics script with opt-out

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,8 @@ export default class Document extends NextDocument {
                     {/*<meta charSet="utf-8" />*/}
                     <meta name="description" content={process.env.siteDescription}/>
                     <meta name="keywords" content={process.env.siteKeywords} />
+                    <script async src="/legal.min.js" data-ackee-server="https://ackee.jacobs-alumni.de" 
+                    data-site-id="1169194f-672e-4057-a17f-7ad566fc966a"></script>
                 </Head>
                 <body>
                     <Main />
